32db0b26ea47edaff5b8b48a194b83ffa219eb05
Getson47 <103142147+Getson47@users.noreply.github.com>
2022-04-25 19:30:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,5 +66,63 @@ test("There are at least 500 words on the page", () => {
 
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+function getWords(type) {
+  // Return words of a given type, or all words if type is 'WORD'
+  let words = [
+    {type: 'ADJECTIVE', value: 'big'},
+    {type: 'ADJECTIVE', value: 'purple'},
+    {type: 'ADJECTIVE', value: 'new'},
+    {type: 'ADJECTIVE', value: 'interesting'},
+    {type: 'ADJECTIVE', value: 'curious'},
+    {type: 'ADJECTIVE', value: 'happy'},
+    {type: 'ADJECTIVE', value: 'busy'},
+    {type: 'ADJECTIVE', value: 'tiny'},
+    {type: 'NOUN', value: 'umbrella'},
+    {type: 'NOUN', value: 'knee'},
+    {type: 'NOUN', value: 'banana'},
+    {type: 'NOUN', value: 'platypus'},
+    {type: 'NOUN', value: 'cat'},
+    {type: 'NOUN', value: 'mouse'},
+    {type: 'NOUN', value: 'house'},
+    {type: 'VERB', value: 'impressed'},
+    {type: 'VERB', value: 'honoured'},
+    {type: 'VERB', value: 'saw'},
+    {type: 'VERB', value: 'ate'},
+    {type: 'VERB', value: 'surprised'},
+    {type: 'VERB', value: 'annoyed'},
+    {type: 'VERB', value: 'touched'},
+    {type: 'VERB', value: 'understood'},
+    {type: 'VERB', value: 'taught'},
+  ];
+  return words.filter(word => type === 'WORD' || word.type === type).map(word => word.value);
+}
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(getNumberOrString(document.getElementById('text').value)));
+
+});
+randomMember(getWords('WORD'));
 
 
